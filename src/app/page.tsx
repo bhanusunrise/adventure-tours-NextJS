8d9e13fb4a49f919aa590d8edbe84f17de227b53
bhanusunrise <pasindubhanukagood@gmail.com>
2024-12-07 02:00:59 +0530

    Contacts and footer fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,14 +188,14 @@ export default function Home() {
 
           <div className="col col-span-3">
             <div className="m-10"></div>
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.269392066728!2d80.7640419!3d6.9775079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3817dd8000e6f%3A0x511d2aeb3484e3ef!2sArun%20Home%20Decor!5e0!3m2!1sen!2slk!4v1733515033206!5m2!1sen!2slk" 
-              style={{border:0, width:"100%", height:"100%"}} 
-              allowFullScreen="true" 
-              loading="lazy" 
-              referrerpolicy="no-referrer-when-downgrade">
+           <iframe 
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.269392066728!2d80.7640419!3d6.9775079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3817dd8000e6f%3A0x511d2aeb3484e3ef!2sArun%20Home%20Decor!5e0!3m2!1sen!2slk!4v1733515033206!5m2!1sen!2slk"
+  style={{ border: 0, width: "100%", height: "100%" }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+></iframe>
 
-              </iframe>
               <div className="m-10"></div>
           </div>
 
