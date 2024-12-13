f92de39121f40e16194f65e0ee6adcda2d70be91
bhanusunrise <pasindubhanukagood@gmail.com>
2024-12-13 15:51:58 +0530

    Value added

--- src/app/components/form/input.tsx
+++ src/app/components/form/input.tsx
@@ -10,6 +10,7 @@ type InputProps = {
   type?: string;
   className?: string;
   accept?: string; // Optional parameter for file types
+  value?: string | number; // Optional value parameter
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // Optional onChange handler
 };
 
@@ -21,6 +22,7 @@ const Input: React.FC<InputProps> = ({
   type = "text",
   className = "",
   accept,
+  value,
   onChange, // Destructuring the onChange handler
 }) => {
   return (
@@ -31,6 +33,7 @@ const Input: React.FC<InputProps> = ({
       min={min}
       max={max}
       accept={accept}
+      value={value} // Apply the value
       onChange={onChange} // Apply the onChange handler
       className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${className}`}
     />
