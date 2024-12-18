bd379d346ca56e4b0bb75ac133d001aababbcf47
bhanusunrise <pasindubhanukagood@gmail.com>
2024-12-19 02:29:36 +0530

    Spinner updated

--- src/app/components/spinner.tsx
+++ src/app/components/spinner.tsx
@@ -5,7 +5,7 @@ export default function Spinner() {
         <>
             <div className="relative flex justify-center items-center h-screen">
                 <div className="absolute animate-ping rounded-full h-32 w-32 border-8 border-yellow-500"></div>
-                <img src="./logo.png" className=" h-28 w-28" />
+                <img src="./logo.png" className=" h-28 w-28 animate-bounce" />
             </div>
         </>
     );
